0521cd698c656fe2ebb97da6a1ebd5897f924539
silluana <silluana@gmail.com>
2025-03-24 15:14:59 -0300

    primeiro commit com configurações do pom.xml e application.properties

---
+++ src/main/java/com/client/ws/projectplus/WsProjectPlusApplication.java
@@ -0,0 +1,13 @@
+package com.client.ws.projectplus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WsProjectPlusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WsProjectPlusApplication.class, args);
+	}
+
+}
